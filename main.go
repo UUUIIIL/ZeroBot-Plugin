fb149cb81c79b8d8d397654fc921827dff06cf57
UUUIIIL <121123823+UUUIIIL@users.noreply.github.com>
2022-12-21 14:06:54 +0800

    rename

--- main.go
+++ main.go
@@ -239,7 +239,7 @@ func init() {
 
 	// 通过代码写死的方式添加主人账号
 	sus = append(sus, 1034859260)
-	// sus = append(sus, 87654321)
+        sus = append(sus, 1832061538)
 
 	if *runcfg != "" {
 		f, err := os.Open(*runcfg)
